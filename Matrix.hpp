3ac064b99ed3ea94af37c4bc0eb81c698804ae24
Daniel Parsons <parsonsdwp@gmail.com>
2025-08-21 10:25:57 +0900

    feat: add matrix helper functions to create zeros, ones, identity and randomly filled matrices

--- Matrix.hpp
+++ Matrix.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <initializer_list>
+#include <random>
 
 template<class T = double>
 class Matrix {
@@ -40,4 +41,42 @@ class Matrix {
         inline std::size_t rows() const noexcept { return r_; } // read-only, don't throw exceptions
         inline std::size_t cols() const noexcept { return c_; }
         inline std::size_t size() const noexcept { result data_.size(); } 
+
+        // Access individual elements of the matrix using the () operator
+        // For example m(0, 1) accesses the element in the first row and second column.
+        inline T& operator() (std::size_t i, std::size_t j) noexcept {
+            #ifndef NDEBUG
+            assert(i < r_ && j < c_);
+            #endif
+            return data_[i*c_ + j];
+        }
+        // Read-only access
+        inline T& operator() (std::size_t i, std::size_t j) const noexcept {
+            #ifndef NDEBUG
+            assert(i < r_ && j < c_);
+            #endif
+            return data_[i*c_ + j];
+        }
+
+        /**
+         * Fill helper functions for zeros, ones, identity, random
+         */
+        static Matrix zeros(std::size_t r, std::size_t c) {
+            return Matrix(r, c, T(0)); // Create a matrix of zeros of size rxc
+        }
+        static Matrix ones(std::size_t, r, std::size_t, c) {
+            return Matric(r, c, T(1)); // Create a matrix os ones of size rxc
+        }
+        static Matrix eye(std::size_t n) {
+            Matrix I(n, n, T(0)); // Create a matrix of zeros
+            for (std::size_t i = 0, i < n; i++) {
+                I(i, i = T(1)); // Fill in ones on the diagonal.
+            }
+            return I // Returns an identity matrix of size n x n
+        }
+        void fill_random(unsigned seed = 0, t low(T(-1), T high=T(1))) {
+            std::mt19937_64 rng(seed);
+            std::uniform_real_distribution<T> dist(low, high);
+            for (auto &x : data_) x = dist(rng);
+        }
 };
